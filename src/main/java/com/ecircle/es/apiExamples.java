8b966aac28f9f66c801b117d895158413807ef17
netankit <ankit.bahuguna@cs.tum.edu>
2014-03-03 18:40:06 +0100

    Added GETAPI to apiExamples.java

---
+++ src/main/java/com/ecircle/es/apiExamples.java
@@ -0,0 +1,31 @@
+package com.ecircle.es;
+
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
+
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.node.Node;
+
+public class apiExamples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// on startup
+		Node node = nodeBuilder().node();
+		Client client = node.client();
+
+		// GET API
+		GetResponse response = client.prepareGet("twitter", "tweet", "1")
+				.execute().actionGet();
+		// Retrieving Index name
+		String _index = response.getIndex();
+		System.out.println("My Index: " + _index);
+
+		String source = response.getSourceAsString();
+		System.out.println("My Source: " + source);
+
+		// On close
+		node.close();
+
+	}
+}
